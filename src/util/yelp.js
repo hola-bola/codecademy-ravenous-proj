d3624cdc13e875ca17cb1286d0d959cc0063e61b
Bola Ajose <bolaa.ajose@gmail.com>
2020-01-06 14:27:41 +0000

    first commit

---
+++ src/util/yelp.js
@@ -0,0 +1,33 @@
+const apiKey = '1Ju-qZfjeBuImCB5RERz5iYA3pmc7PRYyNazcoIz9egm-mrlhEjVYb682xuuxMuQVpERtKkcegxQ6LB70Qh2oAG7iDKuVhsgCOFJ7SanLLEAya0K6t8hJCyrEJAPXnYx';
+
+const Yelp = {
+    searchYelp(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(((business) => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                }));
+            }
+        })
+    }
+
+}
+
+export default Yelp;
